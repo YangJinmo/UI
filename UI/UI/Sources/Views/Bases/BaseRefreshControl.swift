dc5698e7d6b4a1080bcdb4b2be069db36a21140c
Jmy <didwlsah9@gmail.com>
2021-11-16 15:09:13 +0900

    Add BaseRefreshControl

---
+++ UI/UI/Sources/Views/Bases/BaseRefreshControl.swift
@@ -0,0 +1,26 @@
+//
+//  BaseRefreshControl.swift
+//  UI
+//
+//  Created by Jmy on 2021/11/16.
+//
+
+import UIKit
+
+class BaseRefreshControl: UIRefreshControl {
+    // MARK: - Initialization
+    
+    convenience override init() {
+        self.init(frame: .zero)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        tintColor = .systemOrange
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
