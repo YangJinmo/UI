42010f33cc879726da41c0753424acf2df800066
Jmy <didwlsah9@gmail.com>
2021-11-28 23:23:57 +0900

    Add UIStackView extension

---
+++ UI/UI/Sources/Extensions/UIStackView.swift
@@ -0,0 +1,28 @@
+//
+//  UIStackView.swift
+//  UI
+//
+//  Created by Jmy on 2021/11/28.
+//
+
+import UIKit
+
+extension UIStackView {
+    func addArrangedSubviews(_ views: UIView...) {
+        views.forEach { addArrangedSubview($0) }
+    }
+
+    func removeAllArrangedSubviews() {
+        let removedSubviews = arrangedSubviews.reduce([]) { (allSubviews, subview) -> [UIView] in
+            self.removeArrangedSubview(subview)
+            return allSubviews + [subview]
+        }
+
+        for v in removedSubviews {
+            if v.superview != nil {
+                NSLayoutConstraint.deactivate(v.constraints)
+                v.removeFromSuperview()
+            }
+        }
+    }
+}
