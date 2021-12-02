9feb9063fce3db3c00f97323b0f5b8fa8d264075
Jmy <didwlsah9@gmail.com>
2021-12-03 02:06:56 +0900

    Add ImagePresentViewController

---
+++ UI/UI/Sources/ViewControllers/Commons/ImagePresentViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ImagePresentViewController.swift
+//  UI
+//
+//  Created by Jmy on 2021/12/02.
+//
+
+import UIKit
+
+class ImagePresentViewController: BasePresentViewController {
+    // MARK: - Constants
+
+    private let vcName: String = "IU"
+
+    // MARK: - Views
+
+    private let imageView = BaseImageView(
+        image: "https://t1.daumcdn.net/thumb/R600x0/?fname=http%3A%2F%2Ft1.daumcdn.net%2Fqna%2Fimage%2F4b035cdf8372d67108f7e8d339660479dfb41bbd".image
+    )
+    private var imageUrl: String = ""
+
+    // MARK: - Initialization
+
+    convenience init(imageUrl: String) {
+        self.init()
+
+        self.imageUrl = imageUrl
+    }
+
+    // MARK: - View Life Cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        hideDivider()
+        setupScrollableStackView(imageView)
+
+        setTitleLabel(vcName)
+        setImage(imageUrl.image)
+    }
+
+    private func setImage(_ image: UIImage?) {
+        guard let image: UIImage = image else { return }
+        imageView.remakeAspectRatioConstraint(image)
+        imageView.image = image
+    }
+}
