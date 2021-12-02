8631ceb5c3599590e371b75dba0fa982453e7460
Jmy <didwlsah9@gmail.com>
2021-12-03 02:02:53 +0900

    Add UIImage extension

---
+++ UI/UI/Sources/Extensions/UIImage.swift
@@ -0,0 +1,14 @@
+//
+//  UIImage.swift
+//  UI
+//
+//  Created by Jmy on 2021/12/03.
+//
+
+import UIKit
+
+extension UIImage {
+    var aspect: CGFloat {
+        size.width / size.height
+    }
+}
