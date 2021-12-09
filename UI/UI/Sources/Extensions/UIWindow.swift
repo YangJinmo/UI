c504b3f5e4b3b8d9897ed5e4e1e0057bca3e806a
Jmy <didwlsah9@gmail.com>
2021-12-10 00:10:51 +0900

    Add UIWindow extension

---
+++ UI/UI/Sources/Extensions/UIWindow.swift
@@ -0,0 +1,22 @@
+//
+//  UIWindow.swift
+//  UI
+//
+//  Created by Jmy on 2021/12/10.
+//
+
+import UIKit
+
+extension UIWindow {
+    static var key: UIWindow? {
+        if #available(iOS 13, *) {
+            return UIApplication.shared.connectedScenes
+                .filter({ $0.activationState == .foregroundActive })
+                .compactMap({ $0 as? UIWindowScene })
+                .first?.windows
+                .filter({ $0.isKeyWindow }).first
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+}
