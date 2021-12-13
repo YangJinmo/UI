c74c0c927b5471d01f3ed7a9026ed8f98e7446a1
Jmy <didwlsah9@gmail.com>
2021-12-13 23:33:50 +0900

    Add app info

--- UI/UI/Sources/Extensions/Bundle.swift
+++ UI/UI/Sources/Extensions/Bundle.swift
@@ -5,4 +5,60 @@
 //  Created by Jmy on 2021/12/13.
 //
 
-import Foundation
+import UIKit
+
+extension Bundle {
+    class var appName: String {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let value: String = infoDictionary["CFBundleName"] as? String
+        else {
+            return "Unknown"
+        }
+        return value
+    }
+
+    class var appDisplayName: String {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let value: String = infoDictionary["CFBundleDisplayName"] as? String
+        else {
+            return appName
+        }
+        return value
+    }
+
+    class var appVersion: String {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let value: String = infoDictionary["CFBundleShortVersionString"] as? String
+        else {
+            return "Unknown"
+        }
+        return value
+    }
+
+    class var appBuild: String {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let value: String = infoDictionary["CFBundleVersion"] as? String
+        else {
+            return "Unknown"
+        }
+        return value
+    }
+
+    class var bundle: String {
+        guard
+            let infoDictionary = Bundle.main.infoDictionary,
+            let value: String = infoDictionary["CFBundleIdentifier"] as? String
+        else {
+            return "Unknown"
+        }
+        return value
+    }
+
+    class var appVersionAndBuild: String {
+        return "v\(appVersion) (\(appBuild))"
+    }
+}
