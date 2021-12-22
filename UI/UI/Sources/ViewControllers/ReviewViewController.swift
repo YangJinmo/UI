58ea342575902032e6f1971ee401d1dbc46df69b
Jmy <didwlsah9@gmail.com>
2021-12-22 20:20:53 +0900

    Remove toast

--- UI/UI/Sources/ViewControllers/ReviewViewController.swift
+++ UI/UI/Sources/ViewControllers/ReviewViewController.swift
@@ -30,12 +30,6 @@ final class ReviewViewController: BaseViewController {
         setTitleLabel(vcName)
     }
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-         toast("실행 오류\n\n주소가 유효하지 않기 때문에\n해당 페이지를 열 수 없습니다.", bottom: true)
-    }
-
     // MARK: - Methods
 
     private func setupViews() {
