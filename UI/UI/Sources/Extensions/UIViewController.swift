4cc13747974b5c6c27eb335b2f7cbb9d4a72c7cc
Jmy <didwlsah9@gmail.com>
2021-12-23 15:46:21 +0900

    Add UIAlertController

--- UI/UI/Sources/Extensions/UIViewController.swift
+++ UI/UI/Sources/Extensions/UIViewController.swift
@@ -41,6 +41,83 @@ extension UIViewController {
         navigationController?.popToRootViewController(animated: true)
     }
 
+    // MARK: - UIAlertController
+
+    func alert(
+        title: String? = nil,
+        message: String? = nil,
+        confirmHandler: ((UIAlertAction) -> Void)? = nil,
+        completion: (() -> Void)? = nil
+    ) {
+        let alertController: UIAlertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "확인",
+                style: .cancel,
+                handler: confirmHandler
+            )
+        )
+        present(alertController, animated: true, completion: completion)
+    }
+
+    func alertOption(
+        title: String? = nil,
+        message: String? = nil,
+        confirmHandler: ((UIAlertAction) -> Void)? = nil,
+        cancelHandler: ((UIAlertAction) -> Void)? = nil,
+        completion: (() -> Void)? = nil
+    ) {
+        let alertController: UIAlertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "확인",
+                style: .default,
+                handler: confirmHandler
+            )
+        )
+        alertController.addAction(
+            UIAlertAction(
+                title: "취소",
+                style: .cancel,
+                handler: cancelHandler
+            )
+        )
+        present(alertController, animated: true, completion: completion)
+    }
+
+    func actionSheet(
+        title: String? = nil,
+        message: String? = nil,
+        actions: UIAlertAction...,
+        cancelHandler: ((UIAlertAction) -> Void)? = nil,
+        completion: (() -> Void)? = nil
+    ) {
+        let alertController: UIAlertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .actionSheet
+        )
+        actions.forEach {
+            alertController.addAction($0)
+        }
+        alertController.addAction(
+            UIAlertAction(
+                title: "취소",
+                style: .cancel,
+                handler: cancelHandler
+            )
+        )
+        present(alertController, animated: true, completion: completion)
+    }
+
     // MARK: - Toast
 
     func toast(_ text: String, bottom: Bool = false) {
