eec693a9439cba37eeeafb5fce82653bb5e73d2e
Jmy <didwlsah9@gmail.com>
2021-12-23 17:49:31 +0900

    Add alert, actionSheet, action functions

--- UI/UI/Sources/Extensions/UIAlertController.swift
+++ UI/UI/Sources/Extensions/UIAlertController.swift
@@ -7,3 +7,18 @@
 
 import UIKit
 
+extension UIAlertController {
+    static func alert(title: String?, message: String?) -> UIAlertController {
+        return UIAlertController(title: title, message: message, preferredStyle: .alert)
+    }
+
+    static func actionSheet(title: String?, message: String?) -> UIAlertController {
+        return UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+    }
+
+    @discardableResult
+    func action(title: String?, style: UIAlertAction.Style = .default, handler: ((UIAlertAction) -> Void)? = nil) -> UIAlertController {
+        addAction(UIAlertAction(title: title, style: style, handler: handler))
+        return self
+    }
+}
