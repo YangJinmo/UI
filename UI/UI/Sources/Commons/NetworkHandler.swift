d2fba9d84e0325aca1264eaf1301664264526a31
Jmy <didwlsah9@gmail.com>
2021-12-27 00:35:32 +0900

    Init NetworkHandler

---
+++ UI/UI/Sources/Commons/NetworkHandler.swift
@@ -0,0 +1,40 @@
+//
+//  NetworkHandler.swift
+//  UI
+//
+//  Created by Jmy on 2021/12/21.
+//
+
+import Foundation
+
+class NetworkHandler {
+    static func getData(resource: String) {
+        guard let url: URL = resource.url else {
+            "Resource URL is nil".log()
+            return
+        }
+
+        let defaultSession: URLSession = URLSession(configuration: .default)
+        let request: URLRequest = URLRequest(url: url)
+        let dataTask: URLSessionDataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
+            if let error: Error = error {
+                error.localizedDescription.log()
+                return
+            }
+
+            guard
+                let data: Data = data,
+                let response: HTTPURLResponse = response as? HTTPURLResponse, (200 ... 299).contains(response.statusCode)
+            else {
+                return
+            }
+
+            guard let jsonToArray: Any = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                print("json to Any Error")
+                return
+            }
+            print(jsonToArray)
+        }
+        dataTask.resume()
+    }
+}
