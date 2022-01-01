3480afe9c05e28e79b4664bd669bcc68a9a61a1a
Jmy <didwlsah9@gmail.com>
2022-01-01 18:13:00 +0900

    Add isNilOrEmpty functions

---
+++ UI/UI/Sources/Extensions/Optional.swift
@@ -0,0 +1,30 @@
+//
+//  Optional.swift
+//  UI
+//
+//  Created by Jmy on 2022/01/01.
+//
+
+import Foundation
+
+extension Optional where Wrapped == String {
+    var isNilOrEmpty: Bool {
+        switch self {
+        case let string?:
+            return string.isEmpty
+        case nil:
+            return true
+        }
+    }
+}
+
+extension Optional where Wrapped: Collection {
+    var isNilOrEmpty: Bool {
+        switch self {
+        case let collection?:
+            return collection.isEmpty
+        case nil:
+            return true
+        }
+    }
+}
