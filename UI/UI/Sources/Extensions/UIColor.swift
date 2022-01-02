30e2da98533c74ae6f6968a8b9081c636b5a73fe
Jmy <didwlsah9@gmail.com>
2022-01-02 20:19:07 +0900

    Add tint, basic

---
+++ UI/UI/Sources/Extensions/UIColor.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor.swift
+//  UI
+//
+//  Created by Jmy on 2022/01/01.
+//
+
+import UIKit
+
+extension UIColor {
+    static let tint: UIColor = .systemOrange
+    static let basic: UIColor = .label
+}
