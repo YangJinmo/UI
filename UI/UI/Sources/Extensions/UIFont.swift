59f038b3f8d2b73e23b594d777e745d94436947d
Jmy <didwlsah9@gmail.com>
2022-01-02 20:19:20 +0900

    Add title, subtitle

---
+++ UI/UI/Sources/Extensions/UIFont.swift
@@ -0,0 +1,13 @@
+//
+//  UIFont.swift
+//  UI
+//
+//  Created by Jmy on 2022/01/01.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title: UIFont = .systemFont(ofSize: 18, weight: .bold)
+    static let subtitle: UIFont = .systemFont(ofSize: 16, weight: .semibold)
+}
