1bd0c37a29e8431d513280b7dedfc968c451b159
Jmy <didwlsah9@gmail.com>
2022-01-02 20:19:54 +0900

    Add init, makeForTitle, makeForSubtitle, makeForText functions

--- UI/UI/Sources/Extensions/UILabel.swift
+++ UI/UI/Sources/Extensions/UILabel.swift
@@ -8,6 +8,35 @@
 import UIKit
 
 extension UILabel {
+    convenience init(_ text: String? = nil, _ font: UIFont? = nil) {
+        self.init(frame: .zero)
+
+        self.text = text
+        self.font = font
+    }
+
+    static func makeForTitle(_ text: String? = nil) -> UILabel {
+        let label = UILabel(text, .title)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.75
+        return label
+    }
+
+    static func makeForSubtitle(_ text: String? = nil) -> UILabel {
+        let label = UILabel(text, .subtitle)
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.75
+        return label
+    }
+
+    static func makeForText(_ text: String? = nil) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.numberOfLines = 0
+        label.sizeToFit()
+        return label
+    }
+
     /// 행간
     func spaceBetweenTheLines(_ alignment: NSTextAlignment = .center, _ lineSpacing: CGFloat = 2) {
         guard let text = self.text else { return }
