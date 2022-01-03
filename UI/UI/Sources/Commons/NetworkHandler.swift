c0d886f93b29ec70b53fd42a4e8e7150a5c7e047
Jmy <didwlsah9@gmail.com>
2022-01-03 19:35:12 +0900

    Remove NetworkHandler

--- UI/UI/Sources/Commons/NetworkHandler.swift
+++ UI/UI/Sources/Commons/NetworkHandler.swift
@@ -7,34 +7,3 @@
 
 import Foundation
 
-class NetworkHandler {
-    static func getData(resource: String) {
-        guard let url: URL = resource.url else {
-            "Resource URL is nil".log()
-            return
-        }
-
-        let defaultSession: URLSession = URLSession(configuration: .default)
-        let request: URLRequest = url.request
-        let dataTask: URLSessionDataTask = defaultSession.dataTask(with: request) { (data: Data?, response: URLResponse?, error: Error?) in
-            if let error: Error = error {
-                error.localizedDescription.log()
-                return
-            }
-
-            guard
-                let response: HTTPURLResponse = response as? HTTPURLResponse, (200 ... 299).contains(response.statusCode),
-                let data: Data = data
-            else {
-                return
-            }
-
-            guard let jsonToArray: Any = try? JSONSerialization.jsonObject(with: data, options: []) else {
-                print("json to Any Error")
-                return
-            }
-            print(jsonToArray)
-        }
-        dataTask.resume()
-    }
-}
