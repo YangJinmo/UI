c827370ad3df0be4c7d1f2646652cc9c7081ddc2
Jmy <didwlsah9@gmail.com>
2022-01-03 20:14:48 +0900

    Add titleColor parameter

--- UI/UI/Sources/Extensions/UIButton.swift
+++ UI/UI/Sources/Extensions/UIButton.swift
@@ -8,15 +8,22 @@
 import UIKit
 
 extension UIButton {
-    convenience init(_ titleText: String? = nil, _ titleFont: UIFont = .subtitle) {
+    convenience init(
+        _ titleText: String? = nil,
+        _ titleFont: UIFont = .subtitle,
+        _ titleColor: UIColor = .basic
+    ) {
         self.init(frame: .zero)
 
         setTitle(titleText, for: .normal)
-        setTitleColor(.basic, for: .normal)
+        setTitleColor(titleColor, for: .normal)
         titleLabel?.font = titleFont
     }
 
-    convenience init(_ image: UIImage? = nil, _ color: UIColor = .basic) {
+    convenience init(
+        _ image: UIImage? = nil,
+        _ color: UIColor = .basic
+    ) {
         self.init(frame: .zero)
 
         setImage(image, for: .normal)
