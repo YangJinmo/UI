4ffb2133322906ff2bfa0b09c42152d4f987bb16
Jmy <didwlsah9@gmail.com>
2022-01-03 20:44:17 +0900

    init CGFloat extension, Add random()

---
+++ UI/UI/Sources/Extensions/CGFloat.swift
@@ -0,0 +1,14 @@
+//
+//  CGFloat.swift
+//  UI
+//
+//  Created by Jmy on 2022/01/03.
+//
+
+import UIKit
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
