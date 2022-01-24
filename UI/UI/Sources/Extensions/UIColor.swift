ad631ecb64a1963c08465f24646eda424ead120d
zzimss <didwlsah9@gmail.com>
2022-01-24 12:56:04 +0900

    Add rgb, white static functions to UIColor, CGColor extensions

--- UI/UI/Sources/Extensions/UIColor.swift
+++ UI/UI/Sources/Extensions/UIColor.swift
@@ -5,7 +5,7 @@
 //  Created by Jmy on 2022/01/01.
 //
 
-import UIKit
+import UIKit.UIColor
 
 extension UIColor {
     static let tint: UIColor = .systemOrange
@@ -18,5 +18,25 @@ extension UIColor {
             blue: .random(),
             alpha: 1.0
         )
+    }
+}
+
+extension UIColor {
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
+        return .init(red: r / 255, green: g / 255, blue: b / 255, alpha: a)
+    }
+
+    static func white(_ w: CGFloat, a: CGFloat = 1.0) -> UIColor {
+        return .init(white: w / 255, alpha: a)
+    }
+}
+
+extension CGColor {
+    static func rgb(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> CGColor {
+        return UIColor(red: r / 255, green: g / 255, blue: b / 255, alpha: a).cgColor
+    }
+
+    static func white(_ w: CGFloat, a: CGFloat = 1.0) -> CGColor {
+        return UIColor(white: w / 255, alpha: a).cgColor
     }
 }
