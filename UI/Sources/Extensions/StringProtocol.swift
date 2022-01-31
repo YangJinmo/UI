02fb2569a8d11ac89f8ad85c1aa60c3b9d482d9a
zzimss <didwlsah9@gmail.com>
2022-02-01 01:21:01 +0900

    Add StringProtocol extension with firstUppercased, firstCapitalized

---
+++ UI/Sources/Extensions/StringProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  StringProtocol.swift
+//  UI
+//
+//  Created by Jmy on 2022/02/01.
+//
+
+import UIKit
+
+extension StringProtocol {
+    var firstUppercased: String { return prefix(1).uppercased() + dropFirst() }
+    var firstCapitalized: String { return prefix(1).capitalized + dropFirst() }
+}
