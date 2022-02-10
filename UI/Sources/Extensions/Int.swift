aaadbad661813a6ca5a0979d219bea1ccb5cec66
zzimss <didwlsah9@gmail.com>
2022-02-10 18:27:51 +0900

    Add timeFormatted()

--- UI/Sources/Extensions/Int.swift
+++ UI/Sources/Extensions/Int.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension Int {
+    func timeFormatted() -> String {
+        let minutes: Int = (self / 60) % 60
+        let seconds: Int = self % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
+}
