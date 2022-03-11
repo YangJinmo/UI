cde65f9931e113e7a5fbc3221c8e517cb107c8d5
zzimss <didwlsah9@gmail.com>
2022-03-11 09:46:34 +0900

    Add selectionStyle = .none

--- UI/Sources/Views/Bases/BaseTableViewCell.swift
+++ UI/Sources/Views/Bases/BaseTableViewCell.swift
@@ -13,6 +13,7 @@ class BaseTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
 
+        selectionStyle = .none
         initialize()
     }
 
