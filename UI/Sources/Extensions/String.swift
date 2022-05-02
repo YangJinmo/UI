e4138a0c2c414cfeb69aa832eb4656237e262a2a
zzimss <didwlsah9@gmail.com>
2022-05-02 18:39:17 +0900

    Add Regular Expression

--- UI/Sources/Extensions/String.swift
+++ UI/Sources/Extensions/String.swift
@@ -52,6 +52,83 @@ extension String {
         return dateFormatter.date(from: self) ?? Date()
     }
 
+    // MARK: - Regular Expression
+
+    func regexFirstString(_ pattern: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: .anchorsMatchLines)
+            if let _ = regex.firstMatch(in: self, options: .anchored, range: NSRange(location: 0, length: 1)) {
+                return true
+            }
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+        return false
+    }
+
+    func regex(_ pattern: String) -> Bool {
+        do {
+            let regex = try NSRegularExpression(pattern: pattern, options: .allowCommentsAndWhitespace)
+            if let _ = regex.firstMatch(in: self, options: .reportCompletion, range: NSMakeRange(0, count)) {
+                return true
+            }
+        } catch {
+            print(error.localizedDescription)
+            return false
+        }
+        return false
+    }
+
+    // 천지인 키보드의 ‘·(middle dot)’ 허용을 위해서 추가된 코드
+    var hasCharacters: Bool {
+        regex("^[0-9a-zA-Z가-힣ㄱ-ㅎㅏ-ㅣ\\u318D\\u119E\\u11A2\\u2022\\u2025\\u00B7\\uFE55\\u4E10\\u3163\\u3161\\s]$")
+    }
+
+    var hasNumber: Bool {
+        regex("^[0-9]$")
+    }
+
+    var isEmail: Bool {
+        let regEx = "[0-9a-zA-Z._%+-]+@[0-9a-zA-Z.-]+\\.[a-zA-Z]{2,100}"
+        let predicate = NSPredicate(format: "SELF MATCHES %@", regEx)
+
+        return predicate.evaluate(with: self)
+    }
+
+    var isBackspace: Bool {
+        let utf8Char: [CChar]? = cString(using: .utf8)
+        let backspace: Int32 = strcmp(utf8Char, "\\b")
+        return backspace == -92
+    }
+
+    var hasPhoneNumber: Bool {
+        return hasPrefix("010") ? count == 11 : regex("^01(?:0|1|[6-9])(?:\\d{7}|\\d{8})$")
+    }
+
+    var containsWhitespace: Bool {
+        return rangeOfCharacter(from: .whitespacesAndNewlines) != nil
+    }
+
+    var removeWhitespaces: String {
+        return components(separatedBy: .whitespaces).joined()
+    }
+
+    func toPhoneNumber() -> String {
+        return replacingOccurrences(of: "(\\d{3})(\\d{4})(\\d+)", with: " $1-$2-$3", options: .regularExpression, range: nil)
+    }
+
+    func toHiddenPhoneNumber() -> String {
+        return replacingOccurrences(of: "(\\d{3})(\\d{4})(\\d+)", with: " $1-****-$3", options: .regularExpression, range: nil)
+    }
+
+    func toHiddenEmail() -> String {
+        let string = rangeByIndex(start: 0, end: 2)
+        let stringArr = split(separator: "@")
+        let asterisks = String(repeating: "*", count: stringArr[0].count - 3)
+        return string + asterisks + "@" + stringArr[1]
+    }
+
     // MARK: - NSAttributedString
 
     func foregroundColor(_ color: UIColor) -> NSAttributedString {
