3034c3d01b565b9be2619fb60312beb9ab70cbd1
zzimss <didwlsah9@gmail.com>
2022-05-15 19:51:39 +0900

    Update addBorder()

--- UI/Sources/Extensions/CALayer.swift
+++ UI/Sources/Extensions/CALayer.swift
@@ -20,4 +20,35 @@ extension CALayer {
         shadowColor = UIColor.black.cgColor
         shadowOpacity = alpha
     }
+
+    func addBorder(_ edges: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in edges {
+            let border = CALayer()
+
+            switch edge {
+            case .top:
+                border.frame = CGRect(x: 0, y: 0, width: frame.width, height: width)
+                break
+
+            case .bottom:
+                border.frame = CGRect(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+
+            case .left:
+                border.frame = CGRect(x: 0, y: 0, width: width, height: frame.height)
+                break
+
+            case .right:
+                border.frame = CGRect(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+
+            default:
+                break
+            }
+
+            border.backgroundColor = color.cgColor
+
+            addSublayer(border)
+        }
+    }
 }
