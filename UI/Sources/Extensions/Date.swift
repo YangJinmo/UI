3d222d65a6f0c052f4edeab85032be7630a7772e
zzimss <didwlsah9@gmail.com>
2022-05-25 22:21:04 +0900

    Add milliseconds

--- UI/Sources/Extensions/Date.swift
+++ UI/Sources/Extensions/Date.swift
@@ -8,6 +8,10 @@
 import Foundation.NSDate
 
 extension Date {
+    var milliseconds: Int64 {
+        return Int64(timeIntervalSince1970 * 1000)
+    }
+
     func toString(dateFormat: String = "yyyy.MM.dd HH:mm:ss") -> String {
         let formatter = DateFormatter()
         formatter.dateFormat = dateFormat
