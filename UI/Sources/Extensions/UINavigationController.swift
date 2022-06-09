ced8101fef82075a76346e71ec3a701c4e9b2813
zzimss <didwlsah9@gmail.com>
2022-06-09 23:39:52 +0900

    Add functions in UINavigationController extension

--- UI/Sources/Extensions/UINavigationController.swift
+++ UI/Sources/Extensions/UINavigationController.swift
@@ -15,4 +15,33 @@ extension UINavigationController {
         title = tabBarItem.title
         self.tabBarItem = tabBarItem
     }
+
+    func push(controller: UIViewController, animated: Bool = true) {
+        pushViewController(controller, animated: animated)
+    }
+
+    @discardableResult
+    func pop(animated: Bool = true) -> UIViewController? {
+        return popViewController(animated: animated)
+    }
+
+    @discardableResult
+    public func replace(with controller: UIViewController, animated: Bool = true) -> UIViewController? {
+        var controllers = viewControllers
+        let current = controllers.popLast()
+        controllers.append(controller)
+
+        setViewControllers(controllers, animated: animated)
+
+        return current
+    }
+
+    @discardableResult
+    public func replaceAll(with controller: UIViewController, animated: Bool = true) -> [UIViewController] {
+        let currentControllers = viewControllers
+
+        setViewControllers([controller], animated: animated)
+
+        return currentControllers
+    }
 }
