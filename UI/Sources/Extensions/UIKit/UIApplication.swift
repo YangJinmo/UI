c7efe9f343d0b2dd46bd57c447ea029822ecd87f
zzimss <didwlsah9@gmail.com>
2022-06-26 23:52:02 +0900

    Add getMostTopViewController()

--- UI/Sources/Extensions/UIKit/UIApplication.swift
+++ UI/Sources/Extensions/UIKit/UIApplication.swift
@@ -22,4 +22,32 @@ extension UIApplication {
         }
         return controller
     }
+
+    class func getMostTopViewController(base: UIViewController? = nil) -> UIViewController? {
+        var baseVC: UIViewController?
+
+        if base != nil {
+            baseVC = base
+        } else {
+            if #available(iOS 13, *) {
+                baseVC = (UIApplication.shared.connectedScenes
+                    .compactMap { $0 as? UIWindowScene }
+                    .flatMap { $0.windows }
+                    .first { $0.isKeyWindow })?.rootViewController
+            } else {
+                baseVC = UIApplication.shared.keyWindow?.rootViewController
+            }
+        }
+
+        if let naviController = baseVC as? UINavigationController {
+            return getMostTopViewController(base: naviController.visibleViewController)
+
+        } else if let tabbarController = baseVC as? UITabBarController, let selected = tabbarController.selectedViewController {
+            return getMostTopViewController(base: selected)
+
+        } else if let presented = baseVC?.presentedViewController {
+            return getMostTopViewController(base: presented)
+        }
+        return baseVC
+    }
 }
