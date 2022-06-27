772d5590538035917b95de2fb9c740ddacb16791
zzimss <didwlsah9@gmail.com>
2022-06-27 10:10:40 +0900

    Update UIWindow.key

--- UI/Sources/Extensions/UIKit/UIWindow.swift
+++ UI/Sources/Extensions/UIKit/UIWindow.swift
@@ -10,11 +10,7 @@ import UIKit.UIWindow
 extension UIWindow {
     static var key: UIWindow? {
         if #available(iOS 13, *) {
-            return UIApplication.shared.connectedScenes
-                .filter({ $0.activationState == .foregroundActive })
-                .compactMap({ $0 as? UIWindowScene })
-                .first?.windows
-                .filter({ $0.isKeyWindow }).first
+            return UIApplication.shared.windows.first { $0.isKeyWindow }
         } else {
             return UIApplication.shared.keyWindow
         }
