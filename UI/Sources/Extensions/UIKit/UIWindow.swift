be8ee66768c4d9f328ae68277386bf6d665dae5f
zzimss <didwlsah9@gmail.com>
2022-06-27 10:29:01 +0900

    Update UIWindow.key

--- UI/Sources/Extensions/UIKit/UIWindow.swift
+++ UI/Sources/Extensions/UIKit/UIWindow.swift
@@ -10,9 +10,25 @@ import UIKit.UIWindow
 extension UIWindow {
     static var key: UIWindow? {
         if #available(iOS 13, *) {
-            return UIApplication.shared.windows.first { $0.isKeyWindow }
+            return UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }
         } else {
             return UIApplication.shared.keyWindow
+        }
+    }
+}
+
+extension UIApplication {
+    static var keyWindowInConnectedScenes: UIWindow? {
+        if #available(iOS 13, *) {
+            return shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }
+        } else {
+            return shared.keyWindow
         }
     }
 }
