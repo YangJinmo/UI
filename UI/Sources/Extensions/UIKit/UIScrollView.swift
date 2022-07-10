45f2d4c4f4b19d5cbcafe6a9a571b7fea098a201
zzimss <didwlsah9@gmail.com>
2022-07-10 23:04:07 +0900

    Add isReachedBottom()

--- UI/Sources/Extensions/UIKit/UIScrollView.swift
+++ UI/Sources/Extensions/UIKit/UIScrollView.swift
@@ -19,12 +19,15 @@ extension UIScrollView {
         return contentSize.height > bounds.height && bounds.height > 0
     }
 
-    func scrollToTop(down: CGFloat = 0, right: CGFloat = 0, animated: Bool = true) {
-        let point = CGPoint(
-            x: right - contentInset.left,
-            y: down - contentInset.top
-        )
-        setContentOffset(point, animated: animated)
+    func isReachedBottom(withTolerance tolerance: CGFloat = 0) -> Bool {
+        layoutIfNeeded()
+
+        guard isOverflowVertical else {
+            return false
+        }
+
+        let contentOffsetBottom = contentOffset.y + bounds.height
+        return contentOffsetBottom >= contentSize.height - tolerance
     }
 
     func scrollToBottom(up: CGFloat = 0, animated: Bool = true) {
@@ -36,6 +39,14 @@ extension UIScrollView {
 
         let y = contentSize.height - bounds.height + contentInset.bottom + safeAreaInsets.bottom + up
         let point = CGPoint(x: 0, y: y)
+        setContentOffset(point, animated: animated)
+    }
+
+    func scrollToTop(down: CGFloat = 0, right: CGFloat = 0, animated: Bool = true) {
+        let point = CGPoint(
+            x: right - contentInset.left,
+            y: down - contentInset.top
+        )
         setContentOffset(point, animated: animated)
     }
 }
