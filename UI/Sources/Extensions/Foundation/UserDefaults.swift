c1b9400eb74292428b9a0700d8445058058b975d
zzimss <didwlsah9@gmail.com>
2022-07-17 23:41:04 +0900

    Update UserDefaults

--- UI/Sources/Extensions/Foundation/UserDefaults.swift
+++ UI/Sources/Extensions/Foundation/UserDefaults.swift
@@ -8,6 +8,24 @@
 import Foundation
 
 extension UserDefaults {
+    static func save(at value: Any?, forKey key: String) {
+        UserDefaults.standard.set(value, forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+
+    static func get(forKey key: String) -> Any? {
+        return UserDefaults.standard.object(forKey: key)
+    }
+
+    static func remove(forKey key: String) {
+        UserDefaults.standard.removeObject(forKey: key)
+        UserDefaults.standard.synchronize()
+    }
+
+    static func isExist(forKey key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: key) != nil
+    }
+
     func isExist(forKey key: String) -> Bool {
         return object(forKey: key) != nil
     }
