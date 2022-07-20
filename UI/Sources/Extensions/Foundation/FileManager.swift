5e45a38cdf122732d1dfce79bd030b2b6791a2eb
zzimss <didwlsah9@gmail.com>
2022-07-21 00:51:47 +0900

    Add FileManager

---
+++ UI/Sources/Extensions/Foundation/FileManager.swift
@@ -0,0 +1,107 @@
+//
+//  FileManager.swift
+//  UI
+//
+//  Created by Jmy on 2022/07/21.
+//
+
+import Foundation
+
+extension FileManager {
+    class func save(_ data: Data, savePath: String) -> Error? {
+        if FileManager.default.fileExists(atPath: savePath) {
+            do {
+                try FileManager.default.removeItem(atPath: savePath)
+            } catch let error {
+                return error
+            }
+        }
+        do {
+            try data.write(to: URL(fileURLWithPath: savePath))
+        } catch let error {
+            return error
+        }
+        return nil
+    }
+
+    class func save(content: String, savePath: String) -> Error? {
+        if FileManager.default.fileExists(atPath: savePath) {
+            do {
+                try FileManager.default.removeItem(atPath: savePath)
+            } catch let error {
+                return error
+            }
+        }
+        do {
+            try content.write(to: URL(fileURLWithPath: savePath), atomically: true, encoding: .utf8)
+        } catch let error {
+            return error
+        }
+        return nil
+    }
+
+    @discardableResult
+    class func create(at path: String) -> Error? {
+        if !FileManager.default.fileExists(atPath: path) {
+            do {
+                try FileManager.default.createDirectory(atPath: path, withIntermediateDirectories: true, attributes: nil)
+            } catch let error {
+                print("error:\(error)")
+                return error
+            }
+        }
+        return nil
+    }
+
+    @discardableResult
+    class func delete(at path: String) -> Error? {
+        if FileManager.default.fileExists(atPath: path) {
+            do {
+                try FileManager.default.removeItem(atPath: path)
+            } catch let error {
+                return error
+            }
+            return nil
+        }
+        return NSError(domain: "File does not exist", code: -1, userInfo: nil) as Error
+    }
+
+    class func rename(oldFileName: String, newFileName: String) -> Bool {
+        do {
+            try FileManager.default.moveItem(atPath: oldFileName, toPath: newFileName)
+            return true
+        } catch {
+            print("error:\(error)")
+            return false
+        }
+    }
+
+    class func copy(oldFileName: String, newFileName: String) -> Bool {
+        do {
+            try FileManager.default.copyItem(atPath: oldFileName, toPath: newFileName)
+            return true
+        } catch {
+            return false
+        }
+    }
+
+    class var document: String {
+        return NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+    }
+
+    class var library: String {
+        return NSSearchPathForDirectoriesInDomains(.libraryDirectory, .userDomainMask, true)[0]
+    }
+
+    class var temp: String {
+        return NSTemporaryDirectory()
+    }
+
+    class var caches: String {
+        return NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0]
+    }
+
+    class var log: String {
+        return document.appendingPathComponent("Logs")
+    }
+}
