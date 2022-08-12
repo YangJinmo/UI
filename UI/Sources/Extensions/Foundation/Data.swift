d57384af21451eb4f198d1dd90b2588a1d2b96fd
zzimss <didwlsah9@gmail.com>
2022-08-12 20:47:13 +0900

    Add toString, toBase64 in Data

--- UI/Sources/Extensions/Foundation/Data.swift
+++ UI/Sources/Extensions/Foundation/Data.swift
@@ -8,6 +8,14 @@
 import UIKit
 
 extension Data {
+    var toString: String? {
+        String(data: self, encoding: .utf8)
+    }
+
+    var toBase64: String {
+        base64EncodedString(options: .endLineWithLineFeed)
+    }
+
     func decoded<T: Decodable>() throws -> T {
         return try JSONDecoder().decode(T.self, from: self)
     }
