feeac468f305f5bc722b5465705407839e1b629a
zzimss <didwlsah9@gmail.com>
2022-08-13 23:44:38 +0900

    Add return

--- UI/Sources/Extensions/Foundation/Data.swift
+++ UI/Sources/Extensions/Foundation/Data.swift
@@ -9,11 +9,11 @@ import UIKit
 
 extension Data {
     var toString: String? {
-        String(data: self, encoding: .utf8)
+        return String(data: self, encoding: .utf8)
     }
 
     var toBase64: String {
-        base64EncodedString(options: .endLineWithLineFeed)
+        return base64EncodedString(options: .endLineWithLineFeed)
     }
 
     func decoded<T: Decodable>() throws -> T {
