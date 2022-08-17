364c275b83dd520956b381b824634e6067707bb7
zzimss <didwlsah9@gmail.com>
2022-08-17 17:12:11 +0900

    Add deleteAuthenticationCookies(), takeSnapshot(), backList()

--- UI/Sources/Extensions/UIKit/WKWebView.swift
+++ UI/Sources/Extensions/UIKit/WKWebView.swift
@@ -10,7 +10,7 @@ import WebKit.WKWebView
 extension WKWebView {
     // MARK: - Cookies
 
-    func cleanAllCookies() {
+    func removeAllCookies() {
         HTTPCookieStorage.shared.removeCookies(since: Date.distantPast)
 
         WKWebsiteDataStore.default().fetchDataRecords(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes()) { records in
@@ -22,5 +22,36 @@ extension WKWebView {
 
     func refreshCookies() {
         configuration.processPool = WKProcessPool()
+    }
+
+    func deleteAuthenticationCookies() {
+        configuration.websiteDataStore.httpCookieStore.getAllCookies { cookies in
+            for cookie in cookies {
+                if cookie.name == "authentication" {
+                    /// Delete authentication cookies
+                    self.configuration.websiteDataStore.httpCookieStore.delete(cookie)
+                } else {
+                    /// Print cookies
+                    print("\(cookie.name) is set to \(cookie.value)")
+                }
+            }
+        }
+    }
+
+    func takeSnapshot(rect: CGRect = CGRect(x: 0, y: 0, width: 150, height: 50)) {
+        let config = WKSnapshotConfiguration()
+        config.rect = rect
+
+        takeSnapshot(with: config) { image, _ in
+            if let image = image {
+                print(image.size)
+            }
+        }
+    }
+
+    func backList() {
+        for page in backForwardList.backList {
+            print("User visited \(page.url.absoluteString)")
+        }
     }
 }
