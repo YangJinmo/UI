c9bc81b31be6e44a07493a44c391929d32011309
zzimss <didwlsah9@gmail.com>
2022-09-02 00:16:40 +0900

    Update random()

--- UI/Sources/Extensions/UIKit/UIColor.swift
+++ UI/Sources/Extensions/UIKit/UIColor.swift
@@ -9,7 +9,11 @@ import UIKit.UIColor
 
 extension UIColor {
     static func random(a: CGFloat = 1.0) -> UIColor {
-        return .init(red: .random(), green: .random(), blue: .random(), alpha: a)
+        let r = CGFloat(arc4random_uniform(256))
+        let g = CGFloat(arc4random_uniform(256))
+        let b = CGFloat(arc4random_uniform(256))
+        "".log("r: \(r), g: \(g), b: \(b), a: \(a)")
+        return UIColor.rgb(r: r, g: g, b: b, a: a)
     }
 
     static func rgb(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
