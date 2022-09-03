af3da64be7036547a2d09fd538f71dfe8ba11d17
zzimss <didwlsah9@gmail.com>
2022-09-04 07:05:18 +0900

    Remove random() in CGFloat extension

--- UI/Sources/Extensions/Foundation/CGFloat.swift
+++ UI/Sources/Extensions/Foundation/CGFloat.swift
@@ -8,10 +8,6 @@
 import CoreGraphics
 
 extension CGFloat {
-    static func random() -> CGFloat {
-        return CGFloat(arc4random()) / CGFloat(UInt32.max)
-    }
-
     /// 근사값 찾기
     func nearest(inValues values: [CGFloat]) -> CGFloat {
         guard let nearestValue = values.min(by: { abs(self - $0) < abs(self - $1) }) else {
