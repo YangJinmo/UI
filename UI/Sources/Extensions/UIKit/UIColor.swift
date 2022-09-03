3059a370cf3d1f42a9ba4104ddd1b2d7b6a78a33
zzimss <didwlsah9@gmail.com>
2022-09-03 13:24:47 +0900

    Add random() in CGColor extension

--- UI/Sources/Extensions/UIKit/UIColor.swift
+++ UI/Sources/Extensions/UIKit/UIColor.swift
@@ -8,20 +8,20 @@
 import UIKit.UIColor
 
 extension UIColor {
-    static func random(a: CGFloat = 1.0) -> UIColor {
-        let r = CGFloat(arc4random_uniform(256))
-        let g = CGFloat(arc4random_uniform(256))
-        let b = CGFloat(arc4random_uniform(256))
-        "".log("r: \(r), g: \(g), b: \(b), a: \(a)")
-        return UIColor.rgb(r: r, g: g, b: b, a: a)
-    }
-
     static func rgb(r: CGFloat, g: CGFloat, b: CGFloat, a: CGFloat = 1.0) -> UIColor {
         return .init(red: r / 255, green: g / 255, blue: b / 255, alpha: a)
     }
 
     static func white(_ w: CGFloat, a: CGFloat = 1.0) -> UIColor {
         return .init(white: w / 255, alpha: a)
+    }
+
+    static func random(a: CGFloat = 1.0) -> UIColor {
+        let r = CGFloat(arc4random_uniform(256))
+        let g = CGFloat(arc4random_uniform(256))
+        let b = CGFloat(arc4random_uniform(256))
+        "".log("r: \(r), g: \(g), b: \(b), a: \(a)")
+        return rgb(r: r, g: g, b: b, a: a)
     }
 }
 
@@ -32,5 +32,13 @@ extension CGColor {
 
     static func white(_ w: CGFloat, a: CGFloat = 1.0) -> CGColor {
         return UIColor(white: w / 255, alpha: a).cgColor
+    }
+
+    static func random(a: CGFloat = 1.0) -> CGColor {
+        let r = CGFloat(arc4random_uniform(256))
+        let g = CGFloat(arc4random_uniform(256))
+        let b = CGFloat(arc4random_uniform(256))
+        "".log("r: \(r), g: \(g), b: \(b), a: \(a)")
+        return rgb(r: r, g: g, b: b, a: a)
     }
 }
