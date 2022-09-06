a739e5035b7c97a4dd5668f76c3eab16caf81e7a
zzimss <didwlsah9@gmail.com>
2022-09-06 22:00:29 +0900

    Add hex to UIColor property

--- UI/Sources/Extensions/Foundation/String.swift
+++ UI/Sources/Extensions/Foundation/String.swift
@@ -216,6 +216,38 @@ extension String {
         return ceil(size(ofFont: font).height)
     }
 
+    // MARK: - Color
+
+//    var color = "#d3d3d3".toUIColor
+//    var color = "d3d3d3".toUIColor
+//    var color = "d3d3d3".toUIColor.withAlphaComponent(0.5)
+    var toUIColor: UIColor {
+        var cString: String = trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+
+        if cString.hasPrefix("#") {
+            cString.removeFirst()
+        }
+
+        /// wrong hex
+        if cString.count != 6 {
+            return UIColor.gray
+        }
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: cString).scanHexInt64(&rgbValue)
+
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+
+    var toCGColor: CGColor {
+        return toUIColor.cgColor
+    }
+
     // MARK: - NSAttributedString
 
     func foregroundColor(_ color: UIColor) -> NSAttributedString {
