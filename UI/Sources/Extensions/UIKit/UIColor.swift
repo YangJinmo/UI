79c9ef29e1fad874d17351d08c82a55f293f5a45
zzimss <didwlsah9@gmail.com>
2022-09-10 23:42:21 +0900

    Add log()

--- UI/Sources/Extensions/UIKit/UIColor.swift
+++ UI/Sources/Extensions/UIKit/UIColor.swift
@@ -71,10 +71,13 @@ extension UIColor {
             return "000000"
         }
 
-        let r = components[0]
-        let g = components[1]
-        let b = components[2]
-        return String(format: "%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+        let r = Int(components[0] * 255)
+        let g = Int(components[1] * 255)
+        let b = Int(components[2] * 255)
+
+        "r: \(r), g: \(g), b: \(b)".log()
+
+        return String(format: "%02X%02X%02X", r, g, b)
     }
 }
 
