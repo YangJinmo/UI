6650e0756f0982a672ae575efd395ce49b3fce76
zzimss <didwlsah9@gmail.com>
2022-09-10 23:38:27 +0900

    Add hexString

--- UI/Sources/Extensions/UIKit/UIColor.swift
+++ UI/Sources/Extensions/UIKit/UIColor.swift
@@ -65,6 +65,17 @@ extension UIColor {
 
         return rgb(r: CGFloat(r), g: CGFloat(g), b: CGFloat(b), a: a)
     }
+
+    var hexString: String {
+        guard let components = cgColor.components else {
+            return "000000"
+        }
+
+        let r = components[0]
+        let g = components[1]
+        let b = components[2]
+        return String(format: "%02X%02X%02X", Int(r * 255), Int(g * 255), Int(b * 255))
+    }
 }
 
 import CoreGraphics.CGColor
