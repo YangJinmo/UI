b20c6f47839d835646035ecbfcf0822ecd9599f3
zzimss <didwlsah9@gmail.com>
2022-09-12 22:26:06 +0900

    Add LiteralType.swift

---
+++ UI/Sources/Extensions/SwiftStandardLibrary /LiteralType.swift
@@ -0,0 +1,20 @@
+//
+//  LiteralType.swift
+//  UI
+//
+//  Created by Jmy on 2022/09/12.
+//
+
+import CoreGraphics
+
+extension IntegerLiteralType {
+    var f: CGFloat {
+        return CGFloat(self)
+    }
+}
+
+extension FloatLiteralType {
+    var f: CGFloat {
+        return CGFloat(self)
+    }
+}
