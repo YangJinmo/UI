485093eeef3abdb3fafa1f16f9db74f069636a1d
zzimss <didwlsah9@gmail.com>
2022-10-19 17:20:05 +0900

    Add visibleViewController, getVisibleViewControllerFrom()

--- UI/Sources/Extensions/UIKit/UIWindow.swift
+++ UI/Sources/Extensions/UIKit/UIWindow.swift
@@ -8,4 +8,22 @@
 import UIKit.UIWindow
 
 extension UIWindow {
+    var visibleViewController: UIViewController? {
+        return UIWindow.getVisibleViewControllerFrom(rootViewController)
+    }
+
+    static func getVisibleViewControllerFrom(_ viewController: UIViewController?) -> UIViewController? {
+        if let navController = viewController as? UINavigationController {
+            return UIWindow.getVisibleViewControllerFrom(navController.visibleViewController)
+
+        } else if let tabBarController = viewController as? UITabBarController {
+            return UIWindow.getVisibleViewControllerFrom(tabBarController.selectedViewController)
+
+        } else if let presentedVC = viewController?.presentedViewController {
+            return UIWindow.getVisibleViewControllerFrom(presentedVC)
+
+        } else {
+            return viewController
+        }
+    }
 }
