9905921b514fd34ce712a0010edbc831c2cf45fa
zzimss <didwlsah9@gmail.com>
2022-10-19 17:10:35 +0900

    Move mainKeyWindow and so on

--- UI/Sources/Extensions/UIKit/UIApplication.swift
+++ UI/Sources/Extensions/UIKit/UIApplication.swift
@@ -8,6 +8,48 @@
 import UIKit.UIApplication
 
 extension UIApplication {
+    // MARK: - UIWindow
+
+    static var mainKeyWindow: UIWindow? {
+        if #available(iOS 13, *) {
+            return UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first { $0.isKeyWindow }
+        } else {
+            return UIApplication.shared.keyWindow
+        }
+    }
+
+    static var sceneWindow: UIWindow? {
+        guard let sceneDelegate = UIApplication.sceneDelegate else {
+            Log.info("could not get scene delegate ")
+            return nil
+        }
+
+        return sceneDelegate.window
+    }
+
+//    static var appWindow: UIWindow? {
+//        guard let appDelegate = UIApplication.appDelegate else {
+//            Log.info("could not get app delegate ")
+//        }
+//
+//        return appDelegate.window
+//    }
+
+    // MARK: - Delegate
+
+    static var sceneDelegate: SceneDelegate? {
+        return UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
+    }
+
+    static var appDelegate: AppDelegate? {
+        return UIApplication.shared.delegate as? AppDelegate
+    }
+
+    // MARK: - Delegate
+
     class func topViewController(controller: UIViewController? = UIWindow.key?.rootViewController) -> UIViewController? {
         if let navigationController = controller as? UINavigationController {
             return topViewController(controller: navigationController.visibleViewController)
