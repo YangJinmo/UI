8b9addffb1fd0a5c014ffab884c0cf4363eacf0a
zzimss <didwlsah9@gmail.com>
2022-10-21 10:47:22 +0900

    Update Int to UInt32, Add hexa()

--- UI/Sources/Extensions/UIKit/UIColor.swift
+++ UI/Sources/Extensions/UIKit/UIColor.swift
@@ -9,7 +9,7 @@ import UIKit.UIColor
 
 extension UIColor {
 //    let black = UIColor(r: 0x00, g: 0x00, b: 0x00, a: 0.5)
-    convenience init(r: Int, g: Int, b: Int, a: CGFloat = 1.0) {
+    convenience init(r: UInt32, g: UInt32, b: UInt32, a: CGFloat = 1.0) {
         assert(r >= 0 && r <= 255, "Invalid red component")
         assert(g >= 0 && g <= 255, "Invalid green component")
         assert(b >= 0 && b <= 255, "Invalid blue component")
@@ -23,7 +23,7 @@ extension UIColor {
     }
 
 //    let white = UIColor(rgb: 0xFFFFFF, a: 0.5)
-    convenience init(rgb: Int, a: CGFloat = 1.0) {
+    convenience init(rgb: UInt32, a: CGFloat = 1.0) {
         self.init(
             r: (rgb >> 16) & 0xFF,
             g: (rgb >> 8) & 0xFF,
@@ -43,7 +43,7 @@ extension UIColor {
     }
 
 //    let purple = UIColor.hex(0xAB47BC)
-    static func hex(_ hex: Int, a: CGFloat = 1.0) -> UIColor {
+    static func hex(_ hex: UInt32, a: CGFloat = 1.0) -> UIColor {
         let color = (
             r: (hex >> 16) & 0xFF,
             g: (hex >> 08) & 0xFF,
@@ -53,6 +53,20 @@ extension UIColor {
         "r: \(color.r), g: \(color.g), b: \(color.b), a: \(a)".log()
 
         return rgb(r: CGFloat(color.r), g: CGFloat(color.g), b: CGFloat(color.b), a: a)
+    }
+
+//    let purpleA = UIColor.hexa(0xAB47BCAB)
+    static func hexa(_ hexa: UInt32) -> UIColor {
+        let color = (
+            r: (hexa >> 24) & 0xFF,
+            g: (hexa >> 16) & 0xFF,
+            b: (hexa >> 08) & 0xFF,
+            a: CGFloat((hexa >> 00) & 0xFF) / 255
+        )
+
+        "r: \(color.r), g: \(color.g), b: \(color.b), a: \(color.a)".log()
+
+        return rgb(r: CGFloat(color.r), g: CGFloat(color.g), b: CGFloat(color.b), a: color.a)
     }
 
 //    let randomColor = UIColor.random()
@@ -84,7 +98,7 @@ extension UIColor {
 import CoreGraphics.CGColor
 
 extension CGColor {
-    static func hex(_ hex: Int, a: CGFloat = 1.0) -> CGColor {
+    static func hex(_ hex: UInt32, a: CGFloat = 1.0) -> CGColor {
         return UIColor.hex(hex, a: a).cgColor
     }
 
