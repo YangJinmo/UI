5f793abe487cbd578dff7354dadbb3a1a51e2352
zzimss <didwlsah9@gmail.com>
2022-10-30 23:38:43 +0900

    Add removeConstraintsFromSubtree()

--- UI/Sources/Extensions/UIKit/UIView.swift
+++ UI/Sources/Extensions/UIKit/UIView.swift
@@ -599,6 +599,25 @@ extension UIView {
 
     // MARK: - NSLayoutConstraint
 
+    /// Removes specified set of constraints from the views in the receiver's subtree and from the receiver itself.
+    ///
+    /// - parameter constraints: A set of constraints that need to be removed.
+    func removeConstraintsFromSubtree(_ constraints: Set<NSLayoutConstraint>) {
+        var constraintsToRemove = [NSLayoutConstraint]()
+
+        for constraint in self.constraints {
+            if constraints.contains(constraint) {
+                constraintsToRemove.append(constraint)
+            }
+        }
+
+        removeConstraints(constraintsToRemove)
+
+        for view in subviews {
+            view.removeConstraintsFromSubtree(constraints)
+        }
+    }
+
     private func removeConstraint(attribute: NSLayoutConstraint.Attribute) {
         constraints.forEach { constraint in
             if constraint.firstAttribute == attribute {
