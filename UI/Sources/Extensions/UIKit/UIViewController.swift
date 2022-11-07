e756c7961c6432431e63e1e18c80e398ca4c103d
zzimss <didwlsah9@gmail.com>
2022-11-07 20:40:29 +0900

    Add currentViewController()

--- UI/Sources/Extensions/UIKit/UIViewController.swift
+++ UI/Sources/Extensions/UIKit/UIViewController.swift
@@ -99,6 +99,36 @@ extension UIViewController {
 //            || tabBarController?.presentingViewController is UITabBarController
     }
 
+    /// http://stackoverflow.com/questions/24825123/get-the-current-view-controller-from-the-app-delegate
+    func currentViewController() -> UIViewController {
+        func findBestViewController(_ controller: UIViewController?) -> UIViewController? {
+            if let presented = controller?.presentedViewController { // Presented界面
+                return findBestViewController(presented)
+            } else {
+                switch controller {
+                case is UISplitViewController: // Return right hand side
+                    let split = controller as? UISplitViewController
+                    guard split?.viewControllers.isEmpty ?? true else {
+                        return findBestViewController(split?.viewControllers.last)
+                    }
+                case is UINavigationController: // Return top view
+                    let navigation = controller as? UINavigationController
+                    guard navigation?.viewControllers.isEmpty ?? true else {
+                        return findBestViewController(navigation?.topViewController)
+                    }
+                case is UITabBarController: // Return visible view
+                    let tab = controller as? UITabBarController
+                    guard tab?.viewControllers?.isEmpty ?? true else {
+                        return findBestViewController(tab?.selectedViewController)
+                    }
+                default: break
+                }
+            }
+            return controller
+        }
+        return findBestViewController(UIApplication.shared.keyWindow?.rootViewController)!
+    }
+
     // MARK: - Navigation Controller
 
     func presentWithNavigationController(_ rootViewController: UIViewController, completion: (() -> Void)? = nil) {
