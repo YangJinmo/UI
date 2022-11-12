4e3872e7dbd4580145bd4fa5cb01c561f76f3671
zzimss <didwlsah9@gmail.com>
2022-11-12 10:56:37 +0900

    Add queryParameters property

--- UI/Sources/Extensions/Foundation/URL.swift
+++ UI/Sources/Extensions/Foundation/URL.swift
@@ -13,6 +13,23 @@ extension URL {
         return URLRequest(url: self)
     }
 
+    var queryParameters: [String: String]? {
+        guard
+            let components = URLComponents(url: self, resolvingAgainstBaseURL: true),
+            let queryItems = components.queryItems
+        else {
+            return nil
+        }
+
+        var parameters = [String: String]()
+
+        for item in queryItems {
+            parameters[item.name] = item.value
+        }
+
+        return parameters
+    }
+
     // MARK: - UIApplication
 
     var canOpenURL: Bool {
