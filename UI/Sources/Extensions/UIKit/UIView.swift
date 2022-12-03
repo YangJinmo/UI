55c0db6313f28baf2c958f580c2a513b499d1ef6
zzimss <didwlsah9@gmail.com>
2022-12-04 01:05:55 +0900

    Add removeAllConstraints

--- UI/Sources/Extensions/UIKit/UIView.swift
+++ UI/Sources/Extensions/UIKit/UIView.swift
@@ -634,6 +634,27 @@ extension UIView {
         }
     }
 
+    public func removeAllConstraints() {
+        var _superview = superview
+
+        while let superview = _superview {
+            for constraint in superview.constraints {
+                if let first = constraint.firstItem as? UIView, first == self {
+                    superview.removeConstraint(constraint)
+                }
+
+                if let second = constraint.secondItem as? UIView, second == self {
+                    superview.removeConstraint(constraint)
+                }
+            }
+
+            _superview = superview.superview
+        }
+
+        removeConstraints(constraints)
+        translatesAutoresizingMaskIntoConstraints = true
+    }
+
     private func remakeConstraint(_ attribute: NSLayoutConstraint.Attribute, _ constant: CGFloat, _ multiplier: CGFloat) {
         removeConstraint(attribute: attribute)
         addConstraint(
