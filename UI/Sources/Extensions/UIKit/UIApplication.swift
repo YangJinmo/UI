bc172694568e8406a3777a02bfc71b24c109ca82
zzimss <didwlsah9@gmail.com>
2022-12-14 23:15:12 +0900

    Add runInBackground()

--- UI/Sources/Extensions/UIKit/UIApplication.swift
+++ UI/Sources/Extensions/UIKit/UIApplication.swift
@@ -79,4 +79,17 @@ extension UIApplication {
         }
         return baseVC
     }
+
+    func runInBackground(_ closure: @escaping () -> Void, expirationHandler: (() -> Void)? = nil) {
+        DispatchQueue.main.async {
+            let taskID: UIBackgroundTaskIdentifier
+            if let expirationHandler = expirationHandler {
+                taskID = self.beginBackgroundTask(expirationHandler: expirationHandler)
+            } else {
+                taskID = self.beginBackgroundTask(expirationHandler: { })
+            }
+            closure()
+            self.endBackgroundTask(taskID)
+        }
+    }
 }
