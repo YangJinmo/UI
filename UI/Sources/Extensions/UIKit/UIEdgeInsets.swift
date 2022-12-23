5e7b44a7df412b388a12d0d175c003dcf0fae478
zzimss <didwlsah9@gmail.com>
2022-12-23 17:37:57 +0900

    Add UIEdgeInsets.init()

--- UI/Sources/Extensions/UIKit/UIEdgeInsets.swift
+++ UI/Sources/Extensions/UIKit/UIEdgeInsets.swift
@@ -8,6 +8,10 @@
 import UIKit.UIGeometry
 
 extension UIEdgeInsets {
+    init(_ top: CGFloat = 0, left: CGFloat = 0, bottom: CGFloat = 0, right: CGFloat = 0) {
+        self.init(top: top, left: left, bottom: bottom, right: right)
+    }
+
     init(horizontal: CGFloat, vertical: CGFloat) {
         self.init(top: vertical, left: horizontal, bottom: vertical, right: horizontal)
     }
