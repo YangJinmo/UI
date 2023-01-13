8df8591471cde04e21267918c8431b53ae5563cb
zzimss <didwlsah9@gmail.com>
2023-01-13 16:20:23 +0900

    Add digits, LosslessStringConvertible

---
+++ UI/Sources/Extensions/SwiftStandardLibrary/LosslessStringConvertible.swift
@@ -0,0 +1,25 @@
+//
+//  LosslessStringConvertible.swift
+//  UI
+//
+//  Created by Jmy on 2023/01/13.
+//
+
+extension LosslessStringConvertible {
+    var string: String { .init(self) }
+}
+
+extension Numeric where Self: LosslessStringConvertible {
+    /**
+     ````
+     let integer = 123
+     let integerDigits = integer.digits // [1, 2, 3]
+
+     let double = 12.34
+     let doubleDigits = double.digits // [1, 2, 3, 4]
+     ````
+
+     # Link: https://stackoverflow.com/questions/30415937/how-to-split-an-int-to-its-individual-digits
+     */
+    var digits: [Int] { string.digits }
+}
--- UI/Sources/Extensions/SwiftStandardLibrary/StringProtocol.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/StringProtocol.swift
@@ -6,6 +6,18 @@
 //
 
 extension StringProtocol {
+    /**
+      ````
+      let string = "123456"
+      let digits = string.digits // [1, 2, 3, 4, 5, 6]
+      ````
+
+     # Link: https://stackoverflow.com/questions/30415937/how-to-split-an-int-to-its-individual-digits
+      */
+    var digits: [Int] {
+        return compactMap(\.wholeNumberValue)
+    }
+
     var firstUppercased: String {
         return prefix(1).uppercased() + dropFirst()
     }
