2e9afc627fcd9efc72b879e1258e10e2ed114dea
zzimss <didwlsah9@gmail.com>
2023-02-06 21:56:16 +0900

    Add decode in Bundle extension

--- UI/Sources/Extensions/Foundation/Bundle.swift
+++ UI/Sources/Extensions/Foundation/Bundle.swift
@@ -43,4 +43,22 @@ extension Bundle {
     static var iconImage: UIImage? {
         return UIImage(contentsOfFile: iconFilePath)
     }
+
+    func decode<T: Decodable>(_ type: T.Type, from filename: String) -> T {
+        guard let json = url(forResource: filename, withExtension: nil) else {
+            fatalError("Failed to locate \(filename) in app bundle.")
+        }
+
+        guard let jsonData = try? Data(contentsOf: json) else {
+            fatalError("Failed to load \(filename) from app bundle.")
+        }
+
+        let decoder = JSONDecoder()
+
+        guard let result = try? decoder.decode(T.self, from: jsonData) else {
+            fatalError("Failed to decode \(filename) from app bundle.")
+        }
+
+        return result
+    }
 }
