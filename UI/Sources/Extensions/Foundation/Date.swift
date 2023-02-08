dddad6929d94858403c6bb2097fcd9fbf39ad1bf
zzimss <didwlsah9@gmail.com>
2023-02-08 10:15:40 +0900

    Add days function

--- UI/Sources/Extensions/Foundation/Date.swift
+++ UI/Sources/Extensions/Foundation/Date.swift
@@ -236,4 +236,14 @@ extension Date {
         }
         return description
     }
+
+    func days(between otherDate: Date) -> Int {
+        let calendar = Calendar.current
+
+        let startOfSelf = calendar.startOfDay(for: self)
+        let startOfOther = calendar.startOfDay(for: otherDate)
+        let components = calendar.dateComponents([.day], from: startOfSelf, to: startOfOther)
+
+        return abs(components.day ?? 0)
+    }
 }
