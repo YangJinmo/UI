72c8d363ef7565d4c8cb2186321ed750f9812a89
zzimss <didwlsah9@gmail.com>
2023-02-09 23:40:58 +0900

    Add comment in clamp function

--- UI/Sources/Extensions/SwiftStandardLibrary/Comparable.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/Comparable.swift
@@ -6,6 +6,22 @@
 //
 
 extension Comparable {
+    /**
+     ```
+     let number1 = 5
+     print(number1.clamp(low: 0, high: 10))
+     print(number1.clamp(low: 0, high: 3))
+     print(number1.clamp(low: 6, high: 10))
+
+     let number2 = 5.0
+     print(number2.clamp(low: 0, high: 10))
+     print(number2.clamp(low: 0, high: 3))
+     print(number2.clamp(low: 6, high: 10))
+
+     let letter1 = "r"
+     print(letter1.clamp(low: "a", high: "f"))
+     ```
+     */
     func clamp(low: Self, high: Self) -> Self {
         if self > high {
             return high
