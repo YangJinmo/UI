1111adf6ed82a0967937a2003b40f23a835fd7c7
zzimss <didwlsah9@gmail.com>
2023-02-18 23:59:58 +0900

    Add km, ...

--- UI/Sources/Extensions/SwiftStandardLibrary/Double.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/Double.swift
@@ -8,6 +8,12 @@
 import Foundation
 
 extension Double {
+    var km: Double { return self * 1000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    var mm: Double { return self / 1000.0 }
+    var ft: Double { return self / 3.28084 }
+
     func floorNumber(numberOfPlaces: Double = 2.0) -> Double {
         let multiplier = pow(10.0, numberOfPlaces)
         let floored = floor(self * multiplier) / multiplier
