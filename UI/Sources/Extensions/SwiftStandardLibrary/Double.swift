2591bb6e58393953f5a7bb8cfbcad54343c1a238
zzimss <didwlsah9@gmail.com>
2023-02-20 23:42:55 +0900

    Add example in extension Double

--- UI/Sources/Extensions/SwiftStandardLibrary/Double.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/Double.swift
@@ -12,6 +12,21 @@ extension Double {
         return abs(self)
     }
 
+    /**
+     ```
+     let oneInch = 25.4.mm
+     print("One inch is \(oneInch) meters")
+     // Prints "One inch is 0.0254 meters"
+
+     let threeFeet = 3.ft
+     print("Three feet is \(threeFeet) meters")
+     // Prints "Three feet is 0.914399970739201 meters"
+
+     let aMarathon = 42.km + 195.m
+     print("A marathon is \(aMarathon) meters long")
+     // Prints "A marathon is 42195.0 meters long"
+     */
+
     var km: Double { return self * 1000.0 }
     var m: Double { return self }
     var cm: Double { return self / 100.0 }
