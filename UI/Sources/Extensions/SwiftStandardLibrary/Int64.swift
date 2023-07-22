07c5400d23ddfa4db46928c270c062af7fd59b33
zzimss <didwlsah9@gmail.com>
2023-07-22 18:04:02 +0900

    Add millisecondsToSeconds, seconds, millisecondsToTimeString

--- UI/Sources/Extensions/SwiftStandardLibrary/Int64.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/Int64.swift
@@ -15,4 +15,30 @@ extension Int64 {
     var toDate: Date {
         return Date(timeIntervalSince1970: TimeInterval(self) / 1000)
     }
+
+    var millisecondsToSeconds: Int64 {
+        return self / 1000
+    }
+
+    var seconds: Int64 {
+        return self % 60
+    }
+
+    var millisecondsToTimeString: String {
+        let seconds = millisecondsToSeconds
+
+        if seconds >= 60 {
+            var timeString = ""
+
+            timeString += "\(seconds / 60)분"
+
+            if seconds % 60 > 0 {
+                timeString += " \(seconds % 60)초"
+            }
+
+            return timeString
+        } else {
+            return "\(seconds)초"
+        }
+    }
 }
