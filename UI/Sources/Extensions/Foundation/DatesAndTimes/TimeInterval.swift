3c5c7d42948c9ff6ae97fc31d40aa3898e230b62
zzimss <didwlsah9@gmail.com>
2023-12-27 23:35:20 +0900

    Add time

--- UI/Sources/Extensions/Foundation/DatesAndTimes/TimeInterval.swift
+++ UI/Sources/Extensions/Foundation/DatesAndTimes/TimeInterval.swift
@@ -15,4 +15,20 @@ extension TimeInterval {
     var toDate: Date {
         return Date(timeIntervalSince1970: self)
     }
+
+    var milliseconds: Int {
+        return Int(truncatingRemainder(dividingBy: 1) * 1000)
+    }
+
+    var seconds: Int {
+        return Int(self) % 60
+    }
+
+    var minutes: Int {
+        return (Int(self) / 60) % 60
+    }
+
+    var hours: Int {
+        return Int(self) / 3600
+    }
 }
