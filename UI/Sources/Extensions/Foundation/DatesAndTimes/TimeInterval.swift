ee22821cbf15410d1b9f7574fa8ab1a0745631eb
zzimss <didwlsah9@gmail.com>
2023-12-28 19:59:00 +0900

    Add roundedUnixTime

--- UI/Sources/Extensions/Foundation/DatesAndTimes/TimeInterval.swift
+++ UI/Sources/Extensions/Foundation/DatesAndTimes/TimeInterval.swift
@@ -8,26 +8,37 @@
 import Foundation.NSDate
 
 extension TimeInterval {
+    // 1,703,759,397.666739
     static var unixTime: TimeInterval {
         return Date().timeIntervalSince1970
     }
 
+    // 1,703,759,398
+    static var roundedUnixTime: TimeInterval {
+        return Date().timeIntervalSince1970.rounded()
+    }
+
+    // 2023.12.28 19:51:21:105000
     var toDate: Date {
         return Date(timeIntervalSince1970: self)
     }
 
+    // 105
     var milliseconds: Int {
         return Int(truncatingRemainder(dividingBy: 1) * 1000)
     }
 
+    // 21
     var seconds: Int {
         return Int(self) % 60
     }
 
+    // 51
     var minutes: Int {
         return (Int(self) / 60) % 60
     }
 
+    // 473,266
     var hours: Int {
         return Int(self) / 3600
     }
