2b509ae1bfc22e37f5a9b08ffcc5f91f76b38a61
zzimss <didwlsah9@gmail.com>
2024-01-02 15:40:59 +0900

    Remove millisecondsToTimeString

--- UI/Sources/Extensions/SwiftStandardLibrary/Int64.swift
+++ UI/Sources/Extensions/SwiftStandardLibrary/Int64.swift
@@ -19,30 +19,4 @@ extension Int64 {
     var seconds: Int64 {
         return self % 60
     }
-
-    var secondsToMilliseconds: Int64 {
-        return self * 1000
-    }
-
-    var millisecondsToSeconds: Int64 {
-        return self / 1000
-    }
-
-    var millisecondsToTimeString: String {
-        let seconds = millisecondsToSeconds
-
-        if seconds >= 60 {
-            var timeString = ""
-
-            timeString += "\(seconds / 60)분"
-
-            if seconds % 60 > 0 {
-                timeString += " \(seconds % 60)초"
-            }
-
-            return timeString
-        } else {
-            return "\(seconds)초"
-        }
-    }
 }
