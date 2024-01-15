4ab00f567f3b13536ec67a18e92c5175c35410ae
zzimss <didwlsah9@gmail.com>
2024-01-15 19:06:19 +0900

    Add resize function

--- UI/Sources/Extensions/UIKit/UIImage.swift
+++ UI/Sources/Extensions/UIKit/UIImage.swift
@@ -60,4 +60,47 @@ extension UIImage {
         UIGraphicsEndImageContext()
         return outputImage
     }
+
+    func resize(_ size: CGSize, contentMode: UIView.ContentMode = .scaleToFill, quality: CGInterpolationQuality = .medium) -> UIImage? {
+        let horizontalRatio = size.width / self.size.width
+        let verticalRatio = size.height / self.size.height
+        var ratio: CGFloat!
+
+        switch contentMode {
+        case .scaleToFill:
+            ratio = 1
+        case .scaleAspectFill:
+            ratio = max(horizontalRatio, verticalRatio)
+        case .scaleAspectFit:
+            ratio = min(horizontalRatio, verticalRatio)
+        default:
+            ratio = 1
+        }
+
+        let rect = CGRect(x: 0, y: 0, width: size.width * ratio, height: size.height * ratio)
+
+        // Fix for a colorspace / transparency issue that affects some types of
+        // images. See here: http://vocaro.com/trevor/blog/2009/10/12/resize-a-uiimage-the-right-way/comment-page-2/#comment-39951
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: nil, width: Int(rect.size.width), height: Int(rect.size.height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)
+
+        let transform = CGAffineTransform.identity
+
+        // Rotate and/or flip the image if required by its orientation
+        context?.concatenate(transform)
+
+        // Set the quality level to use when rescaling
+        context!.interpolationQuality = quality
+
+        // CGContextSetInterpolationQuality(context, CGInterpolationQuality(kCGInterpolationHigh.value))
+
+        // Draw into the context; this scales the image
+        context?.draw(cgImage!, in: rect)
+
+        // Get the resized image from the context and a UIImage
+        let newImage = UIImage(cgImage: (context?.makeImage()!)!, scale: scale, orientation: imageOrientation)
+        return newImage
+    }
 }
