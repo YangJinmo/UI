f09edd4f52fb3fc18cb4f019bcc9755ba21a22da
zzimss <didwlsah9@gmail.com>
2024-01-18 11:45:36 +0900

    Add resizeImage function

--- UI/Sources/Extensions/UIKit/UIImage.swift
+++ UI/Sources/Extensions/UIKit/UIImage.swift
@@ -61,6 +61,31 @@ extension UIImage {
         return outputImage
     }
 
+    func resizeImage(targetSize: CGSize) -> UIImage? {
+        let widthRatio = targetSize.width / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+
     func resize(_ size: CGSize, contentMode: UIView.ContentMode = .scaleToFill, quality: CGInterpolationQuality = .medium) -> UIImage? {
         let horizontalRatio = size.width / self.size.width
         let verticalRatio = size.height / self.size.height
